5744094900db30541a41b892216628b607ca9302
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 07:47:44 +0530

    added counter heading and buttons +,-,0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 //example to show use of usestate Hook.
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>counter ?</h1>
+      <button>+</button>
+      <button>-</button>
+      <button>0</button>
     </div>
   );
 }
