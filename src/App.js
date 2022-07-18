36afdb712bf4f9a7e8619a1d49fadb5f93d26a8e
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 08:25:43 +0530

    project complete gallery with - added map functionality to img tag

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import './style.css';
 //example to show use of usestate Hook.
 // https://unsplash.com/
 export default function App() {
-  const [a,seta]=React.useState([])
+  const [a, seta] = React.useState([]);
   // a=[]
   // seta=function
 
-  const p1=()=>{
-    const url=prompt("enter image url")
-    seta([...a,url])
-  }
+  const p1 = () => {
+    const url = prompt('enter image url');
+    seta([...a, url]);
+  };
   return (
     <div>
       <button onClick={p1}>add gallery</button>
       <h1>total gallery {a.length}</h1>
-      <img src={} width="100" height="100" alt="gallery" />
+      {a.map((item) => (
+        <img src={item} width="100" height="100" alt="gallery" />
+      ))}
     </div>
   );
 }
