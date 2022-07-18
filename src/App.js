9605cc20859c6cfc96cb58bdbdef8c41aa56d10b
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 08:05:03 +0530

    added html of add more questions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './style.css';
 export default function App() {
   return (
     <div>
+      <button>add more questions </button>
+      <h1>total questions ? </h1>
+      <ol>
+        <li>question </li>
+      </ol>
     </div>
   );
 }
