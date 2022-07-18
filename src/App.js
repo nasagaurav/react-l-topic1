4df95a48d877646845848b3d1569371d9780be6d
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 07:50:48 +0530

    added click event on buttons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './style.css';
 //example to show use of usestate Hook.
 export default function App() {
+
+  const p1=()=>{}
+  const p2=()=>{}
+  const p3=()=>{}
   return (
     <div>
       <h1>counter ?</h1>
-      <button>+</button>
-      <button>-</button>
-      <button>0</button>
+      <button onClick={p1}>+</button>
+      <button onClick={p2}>-</button>
+      <button onClick={p3}>0</button>
     </div>
   );
 }
