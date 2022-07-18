35ad9744dcf21e2d214a75a5494d95a31667e982
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 08:12:11 +0530

    add more question project completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './style.css';
 //example to show use of usestate Hook.
 export default function App() {
+  const [a, seta] = React.useState([]); //[ [] , function ]
+  // a=[]
+  // seta=function
+  // console.log(React.useState([]))
+
+  const p1=()=>{
+      seta(  [...a,'']  )    
+  }
   return (
     <div>
-      <button>add more questions </button>
-      <h1>total questions ? </h1>
+      <button onClick={p1}>add more questions </button>
+      <h1>total questions {a.length} </h1>
       <ol>
-        <li>question </li>
+        {a.map(item=><li>question </li>)}
       </ol>
     </div>
   );
