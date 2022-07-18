a779f7f08d669f98d12c49de72eaadd56ae2ba19
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 08:14:45 +0530

    added  gallery project html

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './style.css';
 export default function App() {
   return (
     <div>
+      <button>add gallery</button>
+      <h1>total gallery ?</h1>
+      <img src={} width="100" height="100" alt="gallery" />
     </div>
   );
 }
