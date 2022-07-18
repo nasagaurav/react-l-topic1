80b6dff78dca59cd1a03b9fdfac8f21906aa5dea
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 08:01:18 +0530

    added increment and decerement useState  with reset to zero

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './style.css';
 //example to show use of usestate Hook.
 export default function App() {
+  const [x, setx] = React.useState(0); // [0,function]
+  // x=0
+  // setx=function
 
-  const p1=()=>{}
-  const p2=()=>{}
-  const p3=()=>{}
+  console.log(React.useState(0));
+
+  const p1 = () => {
+    setx(x+1)
+  };
+  const p2 = () => {
+    setx(x-1)
+  };
+  const p3 = () => {
+    setx(0);//set value of x is 0
+  };
   return (
     <div>
-      <h1>counter ?</h1>
+      <h1>counter {x}</h1>
       <button onClick={p1}>+</button>
       <button onClick={p2}>-</button>
       <button onClick={p3}>0</button>
