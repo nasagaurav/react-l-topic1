9c14c097005c68a7d022f7390d009cc327fdf6ae
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 08:22:52 +0530

    added gallery state ad more function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './style.css';
 //example to show use of usestate Hook.
+// https://unsplash.com/
 export default function App() {
+  const [a,seta]=React.useState([])
+  // a=[]
+  // seta=function
+
+  const p1=()=>{
+    const url=prompt("enter image url")
+    seta([...a,url])
+  }
   return (
     <div>
-      <button>add gallery</button>
-      <h1>total gallery ?</h1>
+      <button onClick={p1}>add gallery</button>
+      <h1>total gallery {a.length}</h1>
       <img src={} width="100" height="100" alt="gallery" />
     </div>
   );
